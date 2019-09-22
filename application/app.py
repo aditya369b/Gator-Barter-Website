ffa6c88226c216a2919e79ab2df2691f343977aa
Alex Kohanim <alexkohanim@gmail.com>
2019-09-22 14:18:35 -0700

    added more for login and registration. in app.py not fully ready for deployment

--- application/app.py
+++ application/app.py
@@ -1,26 +1,49 @@
 """
-Code Taken From: https://github.com/tecladocode/simple-flask-template-app
+Template Taken From: https://github.com/tecladocode/simple-flask-template-app by Alex Kohanim
+More blog posts from the original author: https://blog.tecladocode.com/
+Might incorperate some features mentioned in the blog post(s)
+Also, this blog post: https://blog.tecladocode.com/handling-the-next-url-when-logging-in-with-flask/
 """
 
 from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
+users = []
 
-
+# name=session.get("username", "Unknown")
 @app.route("/")
 def home():
-    return render_template("home.html", name=session.get("username", "Unknown"))
+    return render_template("home.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        next_url = request.form.get("next")
+
+        if username in users and users[username][1] == password:
+            session["username"] = username
+            if next_url:
+                return redirect(next_url)
+            return redirect(url_for("profile"))
     return render_template("login.html")
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    # Here you could register the user.
-    # Add them to a database, for example.
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        next_url = request.form.get("next")
+
+    if username not in users: # new user
+        session["username"] = username
+        session["password"] = password
+        if next_url:
+            return redirect(next_url)
+        return redirect(url_for("profile"))
     return render_template("register.html")
 
 @app.route("/about")
