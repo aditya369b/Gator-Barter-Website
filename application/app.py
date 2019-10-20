1862fff8bbb67edd7ef17c238363bcb3d866daa1
Akshay Kasar <akshaypkasar@gmail.com>
2019-10-20 10:23:42 -0700

    changes of signin_signup

--- application/app.py
+++ application/app.py
@@ -6,9 +6,24 @@ Also, this blog post: https://blog.tecladocode.com/handling-the-next-url-when-lo
 """
 
 from flask import Flask, render_template, request, session, redirect, url_for
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+
+# Database connection info. Note that this is not a secure connection.
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'gatorbarter'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+#initialize the db connection
+mysql = MySQL()
+mysql.init_app(app)
+conn = mysql.connect()
+cursor = conn.cursor()
+
 users = []
+d = None
 
 # name=session.get("username", "Unknown")
 @app.route("/")
@@ -16,13 +31,35 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
+    if request.method == "POST":
+        username = request.form['username']
+        pswd = request.form['password']    
+        print(username, " tried to login")
+        
+        data = cursor.execute("SELECT * FROM user where u_email = %s", (username))
+        print(data)
+        conn.commit()
+        return render_template("home.html",code=200)
+    
+    print("Simple Login Page Click")
     return render_template("login.html")
 
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
+    if request.method == "POST":
+        username = request.form['username']
+        pswd = request.form['password']    
+        print(username, " tried to register")
+        
+        d = cursor.execute("INSERT INTO user (u_email, u_pass, u_fname, u_lname) Values (%s, %s, %s, %s)", (username, pswd, "akshay", "kasar"))
+        print(d)
+        conn.commit()
+        return render_template("home.html")
+    
+    print("Simple Register Page Click")
     return render_template("register.html")
 
 @app.route("/about")
