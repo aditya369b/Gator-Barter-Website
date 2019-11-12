ebab5d2b19b8567a4cb1f9c03a60c7fcf0cd22c7
Alex Kohanim <alexkohanim@gmail.com>
2019-11-11 18:18:10 -0800

    resolved some formatting and variable names

--- application/gatorUser.py
+++ application/gatorUser.py
@@ -3,20 +3,19 @@ from flask import json, jsonify
 import time
 
 
-
 class User():
     def __init__(
-            self, 
-            u_id, 
-            u_email, 
+            self,
+            u_id,
+            u_email,
             u_pwd,
             u_is_admin,
-            u_create_ts,# = time.strftime('%Y-%m-%d %H:%M:%S'), 
-            u_update_ts,# = time.strftime('%Y-%m-%d %H:%M:%S'),
+            u_create_ts,  # = time.strftime('%Y-%m-%d %H:%M:%S'),
+            u_update_ts,  # = time.strftime('%Y-%m-%d %H:%M:%S'),
             u_fname,
             u_lname,
-            u_status = 1):
-        
+            u_status=1):
+
         self.u_id = u_id
         self.u_email = u_email
         self.u_fname = u_fname
@@ -27,17 +26,14 @@ class User():
             self.u_create_ts = time.strftime('%Y-%m-%d %H:%M:%S')
             self.u_update_ts = time.strftime('%Y-%m-%d %H:%M:%S')
         else:
-            self.u_created_ts = u_created_ts
-            self.u_updated_ts = u_updated_ts     
+            self.u_created_ts = u_create_ts
+            self.u_updated_ts = u_update_ts
         self.u_status = u_status
-        
+
     def isAdmin(self):
         return self.u_is_admin > 0
+
 
 def makeUser(userTuple):
     return User(*userTuple)
 # -*- coding: utf-8 -*-
-
-
-
-
