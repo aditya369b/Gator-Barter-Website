fb81e2a9c45e3ebbe45dc694b35fa53d0c677488
Philip Yu <pyu3@mail.sfsu.edu>
2019-11-13 16:23:21 -0800

    Update nav bar for logged in users

--- application/app.py
+++ application/app.py
@@ -21,7 +21,6 @@ import time
 import os
 
 # from livereload import Server   # PHILIPTEST
-# from livereload import Server   # PHILIPTEST
 
 
 app = Flask(__name__)
@@ -32,8 +31,6 @@ app.config['MYSQL_DATABASE_DB'] = 'gatorbarter'
 app.config['MYSQL_DATABASE_HOST'] = '0.0.0.0'
 # app.config['DEBUG'] = 'True'    # PHILIPTEST
 app.secret_key = os.urandom(32)
-
-# app.config['DEBUG'] = 'True'    # PHILIPTEST
 
 # Master Connection, Server ready, don't push changes.
 db = pymysql.connect(app.config['MYSQL_DATABASE_HOST'],
@@ -379,7 +376,6 @@ def not_found(e):
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0")
 #    server = Server(app.wsgi_app)   # PHILIPTEST
 #    server.serve()  # PHILIPTEST
-    #  app.run("0.0.0.0")
+     app.run("0.0.0.0")
