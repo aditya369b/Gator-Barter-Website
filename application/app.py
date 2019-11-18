d3dc06ece6797e699269686e5bc1d08aba5d137a
AlexKohanim <alexkohanim@gmail.com>
2019-11-17 21:16:31 -0800

    fixed login bug

--- application/app.py
+++ application/app.py
@@ -159,6 +159,7 @@ def productPage(product_id):
 
 @app.route("/categories/<categoryName>", methods=["POST", "GET"])
 def selectCategory(categoryName):
+    sessionUser = "" if 'sessionUser' not in session else session['sessionUser']
 
     cursor = getCursor()[1]
     print(categoryName)
@@ -177,7 +178,7 @@ def selectCategory(categoryName):
             productObject = product.makeProduct(d)
             productList.append(productObject)
 
-    return render_template("home.html", products=productList, feedback=categoryName)
+    return render_template("home.html", products=productList, feedback=categoryName, sessionUser=sessionUser)
 
 
 @app.route("/login", methods=['GET', 'POST'])
